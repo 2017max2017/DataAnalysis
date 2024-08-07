fcf7dc5338fb4a5d899ce5affd2bafac626b769f
2017max2017 <miaolu2012@gmail.com>
2024-08-07 23:01:19 +0800

    Add alldataanalysis.py

--- alldataanalysis.py
+++ alldataanalysis.py
@@ -17,8 +17,9 @@ connection_string = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};D
 batch_size = 1000
 
 # 读取公司名称列表
-file_path = '/home/user/data
-/公司名称0727.xlsx'
+file_path = '/home/user/data/公司名称0727.xlsx'  # 文件路径
+
+
 data = pd.read_excel(file_path, sheet_name='Sheet1', dtype={'Company Name': str})
 
 # 假设 data 是你的 DataFrame
